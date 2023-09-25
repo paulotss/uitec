f9e9874cf8883953b19b12bec01ba43269540bb8
Paulo de Tarso <paulo.oinab@gmail.com>
2023-09-25 13:59:30 -0300

    add home component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
   title = 'uitec_front';
